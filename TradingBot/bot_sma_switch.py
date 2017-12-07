34716e551a8a50c74c33e50689f0d7f820d845c3
MHR741 <34139588+MHR741@users.noreply.github.com>
2017-12-07 00:50:47 -0600

    SMA logic change
    SMA logic change

--- TradingBot/bot_sma_switch.py
+++ TradingBot/bot_sma_switch.py
@@ -83,9 +83,9 @@ while order_book.message_count < 1000000000000:
     if long_sma != None:
         if my_MA.count > 30:
             short_sma =  my_MA.get_sma(30*60)
-            if (order_book.net_position > 4 and short_sma - long_sma < 1) or (order_book.net_position < -4 and short_sma - long_sma > 1):
+            if (order_book.net_position > 4 and short_sma - long_sma < -5) or (order_book.net_position < -4 and short_sma - long_sma > 5):
                 use_long_sma = False
-            elif abs(long_sma-short_sma) < 1:
+            elif abs(long_sma-short_sma) < 5:
                 use_long_sma = True
             
             if use_long_sma:
@@ -95,7 +95,7 @@ while order_book.message_count < 1000000000000:
                 
             order_book.valid_sma = True
             order_book.short_std = my_MA.get_weighted_std(5*60) * 2 
-            order_book.long_std = my_MA.get_weighted_std(30*60)
+            order_book.long_std = my_MA.get_weighted_std(30*60) / 2
             logger.info('Price: {:.2f}\tPnL: {:.2f}\tNP: {:.1f}\tSMA: {:.2f}\tBid Theo: {:.2f}\tAsk Theo: {:.2f}\t5_wStd: {:.2f}\t30_wStd: {:.2f}\tlSMA: {:.2f}\tsSMA: {:.2f}'.format(Decimal(order_book.trade_price), order_book.get_pnl(), order_book.net_position, order_book.sma, order_book.bid_theo, order_book.ask_theo, order_book.short_std, order_book.long_std, long_sma, short_sma))
         
         else:
