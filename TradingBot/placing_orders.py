fea2ff4096da5e9536688e785b925985e75da3de
piotruslalka <peteyl@umich.edu>
2017-12-07 09:34:07 -0600

    saving work

--- TradingBot/placing_orders.py
+++ TradingBot/placing_orders.py
@@ -18,9 +18,14 @@ auth_client = MyFillOrderBook(myKeys['key'], myKeys['secret'], myKeys['passphras
 #my_ask = Decimal(close_price) + offset
 #my_bid = Decimal(close_price) - offset
 
-my_ask = 13453+3.34
 
-my_order = auth_client.place_my_limit_order('sell', my_ask, '0.01')
-#my_order = auth_client.place_my_limit_order('buy', my_bid, '0.01')
 
-print(my_order)
+
+    
+my_bid = 16040.11
+
+my_order = auth_client.place_my_limit_order('buy', my_bid, '0.01')
+
+order_details = auth_client.my_sell_orders[0]
+
+print(order_details)
