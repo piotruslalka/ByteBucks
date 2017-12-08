53da09e086305e3d3bd014e21d4a2e6746872f74
MHR741 <34139588+MHR741@users.noreply.github.com>
2017-12-08 04:17:09 -0600

    ActiveOrderLogger
    Log current active orders to keep connection open.

---
+++ TradingBot/ActiveOrderLogger.py
@@ -0,0 +1,37 @@
+# Here we test how to place orders
+
+import time
+import datetime as dt
+import config
+import sys
+import logging
+import numpy as np
+import types
+
+import gdax
+
+from gdax import OrderBook
+from decimal import Decimal
+
+
+# Print out my Keys
+my_user_id = config.my_user_id
+myKeys = config.live
+
+count = 0
+while count < 1000000000000:
+	print(count)
+	auth_client = gdax.AuthenticatedClient(myKeys['key'], myKeys['secret'], myKeys['passphrase'] )
+	my_accounts = auth_client.get_accounts()
+	print(my_accounts)
+	
+	my_position = auth_client.get_position()
+	print(my_position)
+	
+	my_orders = auth_client.get_orders(product_id='BTC-USD', status = "open")
+	if isinstance(my_orders, types.GeneratorType):
+		for order in my_orders:
+			print(order)
+	print(my_orders)
+	time.sleep(5*60)
+	count += 1
