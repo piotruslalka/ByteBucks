b1bdf85d2f8c9ddb5a50740eb638b5a9ee0fd9f4
piotruslalka <peteyl@umich.edu>
2017-12-14 01:14:22 -0600

    Strategy Settings in file not config

--- TradingBot/bot_sma_switch.py
+++ TradingBot/bot_sma_switch.py
@@ -10,6 +10,23 @@ from moving_average import MovingAverageCalculation
 from OrderBook import OrderBookConsole
 from datetime import datetime
 
+# Strategy Settings: Package Trade Settings as a dictionary so you can simply pass that into OrderBook
+strategy_settings = {
+    'strategy_name': "bot_sma_switch",
+    'order_size': 0.001,
+    'buy_initial_offset': 1,
+    'sell_initial_offset': 1,
+    'buy_additional_offset': 1,
+    'sell_additional_offset': 1,
+    'max_long_position': 100,
+    'max_short_position': 100,
+    'fill_notifications': True,
+    'place_notifications': False,
+    'connection_notifications': True,
+}
+
+
+
 # Logging Settings
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
