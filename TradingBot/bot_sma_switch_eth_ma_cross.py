5c4b5d0eb8a22b96818b65432c2899a8d55cfa5b
piotruslalka <peteyl@umich.edu>
2018-01-30 14:54:07 -0600

    prod 2018-01-30

--- TradingBot/bot_sma_switch_eth_ma_cross.py
+++ TradingBot/bot_sma_switch_eth_ma_cross.py
@@ -15,7 +15,7 @@ strategy_settings = {
     'strategy_name': "bot_sma_cross_stable",
     'order_size': 0.01,
     'set_ma_value': True,
-    'manual_ma_value': 969.69,
+    'manual_ma_value': 1050.69,
     'min_size_for_order_update': 0,
     'min_distance_for_order_update': 0,
     'buy_initial_offset': 5,
@@ -80,13 +80,13 @@ my_MA = MovingAverageCalculation(period=strategy_settings.get('sma_long_duration
 
 # Start Up OrderBook
 order_book = OrderBookConsole(product_id=strategy_settings.get('product_id'), keys=myKeys, strategy_settings = strategy_settings)
-order_book.auth_client.buy_levels = 0
-order_book.auth_client.net_position = -1
-current_price = 986.00
-current_pnl = -0.03
-order_book.auth_client.real_position = strategy_settings.get('order_size') * order_book.auth_client.net_position
-order_book.auth_client.pnl = current_pnl - (order_book.auth_client.real_position * current_price)
-order_book.auth_client.sell_levels = order_book.auth_client.buy_levels - order_book.auth_client.real_position
+#order_book.auth_client.buy_levels = 0
+#order_book.auth_client.net_position = -1
+#current_price = 986.00
+#current_pnl = -0.03
+#order_book.auth_client.real_position = strategy_settings.get('order_size') * order_book.auth_client.net_position
+#order_book.auth_client.pnl = current_pnl - (order_book.auth_client.real_position * current_price)
+#order_book.auth_client.sell_levels = order_book.auth_client.buy_levels - order_book.auth_client.real_position
 order_book.auth = True
 order_book.api_key = myKeys['key']
 order_book.api_secret = myKeys['secret']
