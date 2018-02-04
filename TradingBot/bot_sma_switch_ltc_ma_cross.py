4ba3f551874bf7ffa513c3d7f2c73a4142ea5f72
piotruslalka <peteyl@umich.edu>
2018-02-04 11:15:39 -0600

    2018-02-04 LTC Production

--- TradingBot/bot_sma_switch_ltc_ma_cross.py
+++ TradingBot/bot_sma_switch_ltc_ma_cross.py
@@ -12,17 +12,18 @@ from datetime import datetime
 # Strategy Settings: Package Trade Settings as a dictionary so you can simply pass that into OrderBook
 strategy_settings = {
     'product_id': 'LTC-USD',
-    'strategy_name': "bot_sma_cross_stable",
-    'order_size': 0.153,
+    'min_tick': 0.1,
+    'strategy_name': "bot_ltc_cross_stable",
+    'order_size': 0.1,
     'set_ma_value': True,
-    'manual_ma_value': 215,
+    'manual_ma_value': 150.69,
     'min_size_for_order_update': 0,
     'min_distance_for_order_update': 0,
-    'buy_initial_offset': 1.5,
-    'sell_initial_offset': 3,
-    'sma_cross_diff': 0.15,
-    'break_out_level_add': 15,
-    'break_out_level_reduce': 15,
+    'buy_initial_offset': 1.0,
+    'sell_initial_offset': 5.0,
+    'sma_cross_diff': 0,
+    'break_out_level_add': 5,
+    'break_out_level_reduce': 5,
     'buy_max_initial_profit_target': 50000,
     'sell_max_initial_profit_target': 50000,
     'max_long_position': 10000,
@@ -60,11 +61,11 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 # Create Debug file handler and set level to DEBUG
-handler = logging.FileHandler(os.path.join("C:", "debug_" + strategy_settings.get('strategy_name') + "_" + time.strftime("%Y%m%d_%H%M%S") + ".log"),"w")
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+# handler = logging.FileHandler(os.path.join("C:", "debug_" + strategy_settings.get('strategy_name') + "_" + time.strftime("%Y%m%d_%H%M%S") + ".log"),"w")
+# handler.setLevel(logging.DEBUG)
+# formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
 
 
 # Log my Keys
@@ -80,13 +81,14 @@ my_MA = MovingAverageCalculation(period=strategy_settings.get('sma_long_duration
 
 # Start Up OrderBook
 order_book = OrderBookConsole(product_id=strategy_settings.get('product_id'), keys=myKeys, strategy_settings = strategy_settings)
-order_book.auth_client.buy_levels = 1.071
-order_book.auth_client.net_position = 7
-current_price = 189.34
-current_pnl = 12.2
-order_book.auth_client.real_position = strategy_settings.get('order_size') * order_book.auth_client.net_position
-order_book.auth_client.pnl = current_pnl - (order_book.auth_client.real_position * current_price)
-order_book.auth_client.sell_levels = order_book.auth_client.buy_levels - order_book.auth_client.real_position
+#order_book.auth_client.buy_levels = 1.071
+#order_book.auth_client.net_position = 7
+#current_price = 189.34
+#current_pnl = 12.2
+#order_book.auth_client.real_position = strategy_settings.get('order_size') * order_book.auth_client.net_position
+#order_book.auth_client.pnl = current_pnl - (order_book.auth_client.real_position * current_price)
+#order_book.auth_client.sell_levels = order_book.auth_client.buy_levels - order_book.auth_client.real_position
+
 order_book.auth = True
 order_book.api_key = myKeys['key']
 order_book.api_secret = myKeys['secret']
